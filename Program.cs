4c690dd0d7a1b40b4fc70011b5328eae2797e419
braaxL <135743603+braaxL@users.noreply.github.com>
2023-06-06 16:35:27 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Uma Mensagem De Teste");
+            Console.WriteLine("Estou Enviando Para o GitHub");
+            Console.ReadKey();
+
+
+        }
+    }
+}
